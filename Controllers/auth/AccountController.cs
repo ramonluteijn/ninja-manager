ed18963ab53ed301d87e346e235beef711c9891d
ramon luteijn <ramon@intractief.nl>
2024-09-26 19:45:47 +0200

    login pages

---
+++ Controllers/auth/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ninjamanager.Controllers.auth;
+
+public class AccountController : Controller
+{
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    public IActionResult Logout()
+    {
+        return Redirect("/");
+    }
+}
